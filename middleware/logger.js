3c09fdc183cfce24a7ecd595f3c326f6aa8cb095
Raj Rathod <rajrasikrathod@gmail.com>
2022-10-01 16:09:02 +1300

    Chapter 2 40:33

---
+++ middleware/logger.js
@@ -0,0 +1,45 @@
+// { format } - this is destructuring format from the date-fns dependency
+const { format } = require('date-fns')
+// { v4: uuid } - this is destructuring v4 and renaming it uuid from the uuid dependency 
+const { v4 : uuid } = require('uuid')
+// fs module for file system - comes from node since it's already built in
+const fs = require('fs')
+const fsPromises = require('fs').promises
+const path = require('path')
+
+const logEvents = async(message, logFileName) => {
+    // These are template literals
+    // Whenever a log message is created you are getting the date and time 
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}` // new date is being formatted to meet what's on the right, the 'yyyMMdd\' stuff
+    // In the log item, passing in the dateTime just created, and the \t are made so it's easy to import into excel
+    // uuid creates a specific id for each log item, and the message
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+
+    try {
+        // see if directory exists first
+        if(!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            // if directory doesn't exist, make it
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
+        }
+        // logItem is to log item to the file which is logFileName
+        await fsPromises.appendFile((path.join(__dirname, '..', 'logs', logFileName), logItem))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// The middleware
+// middleware has a request, response, and  the ability to call next to move onto the next middleware
+const logger = (req, res, next) => {
+    // logEvents was created above
+    // Origin = where you the request originated from  
+    // writing into reqLog.log which is like a text file but for logs, can open like a text file
+    // does it for every single request, but can put conditions like only logging if it's not coming from
+    // our own url or specific request methods. 
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
+    // This will show in the log of node.js as running server
+    console.log(`${req.method} ${req.path}`)
+    next() // to move onto next piece of middleware or eventually the controller where the request would be processed, the logger would come first before those things
+}
+
+module.exports { logEvents, logger } // both are exported, since we might want to use log events inside of an error handler
--- server.js
+++ server.js
@@ -8,12 +8,37 @@ const path = require('path')
 // in the environment variable, 
 // otherwise run it locally at PORT 3500
 const PORT = process.env.PORT || 3500
+
+app.use(express.json())
 // the '/' is usually the root or index of a web page.
 // __dirname is a global variable that node.js understands and it says look inside of the folder we're in
 // then look inside the public folder.
 // we're telling express where to find static files like a css file or image 
-app.use('/', express.static(path.join(__dirname, '/public')))
+// express.static is built-in middleware, it tells the server where to grab static files.
+app.use('/', express.static(path.join(__dirname, '/public'))) // you don't have to use the forward slash
+
+// looking for a routes folder then a root file.
+app.use('/', require('./routes/root'))
+
+// instead of app.use so it listens for the astrix which basically means 'all' which is routed to this instead of anything else.
+app.all('*', (req, res) => {
+    res.status(404)
+    // Headers from the request that comes in
+    // different responses for different types of requests
+    if(req.accepts('html'))
+        res.sendFile(path.join(__dirname, '/views', '404.html'))
+    else if (req.accepts('json'))
+        res.json({message: '404 Not Found'})
+    else
+        res.type('txt').send('404 Not Found')
+})
 
 // () is a function and => is saying within this function do this.
 // A template literal is what is within the console.log function.
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
+//css folder is made in public folder
+
+// normally with REST API we're just gonna be receiving requests and sending back json data when requested
+// however a rest api can still have a splash page and return information about requests that cannot be fulfilled
